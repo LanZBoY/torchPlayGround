4d2fe16b82cca9195851a1c97edce006117d4f7d
WenTee <superjimalex@gmail.com>
2022-10-01 15:23:10 +0800

    Word Embedding for TextClassification

--- NLP/NLPUtils/preprocessUtils.py
+++ NLP/NLPUtils/preprocessUtils.py
@@ -1,9 +1,11 @@
 import os
 from typing import Iterable
+import string
+
+def removePunctuation(sentence : str):
+        return str.translate(sentence, str.maketrans('','', string.punctuation))
+
 def removeWord(removeWordList: list, sentence : Iterable):
     for removeWord in removeWordList:
         sentence = list(filter(removeWord.__ne__,sentence))
     return sentence
-
-if __name__ == '__main__':
-    print(removeWord(removeWordList='./NLP/NLPUtils/english.txt', sentence=['not', 'man', 'dare' , 'b']))
