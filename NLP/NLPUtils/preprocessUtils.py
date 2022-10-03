7d06920f3cb14edc9ce8bc7f76fbdc0a01d0bc58
WenTee <superjimalex@gmail.com>
2022-10-03 15:16:24 +0800

    變更專案目錄結構

--- NLP/NLPUtils/preprocessUtils.py
+++ NLP/NLPUtils/preprocessUtils.py
@@ -5,7 +5,7 @@ import string
 def removePunctuation(sentence : str):
         return str.translate(sentence, str.maketrans('','', string.punctuation))
 
-def removeWord(removeWordList: list, sentence : Iterable):
+def removeWord(removeWordList: list, sentence : list):
     for removeWord in removeWordList:
         sentence = list(filter(removeWord.__ne__,sentence))
     return sentence
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # torchPlayGround
-目前這個專案還在研究階段
+
+這個專案是給想學pytorch的人建立起來的專案，很多基本教學都在跟目錄中
+可以將這個專案fork下來後執行。
+
+## 在執行前請先輸入以下指令
+```cmd=
+pip install -r requirement.txt
+```
+用來安裝這個專案所有會用到的套件。
